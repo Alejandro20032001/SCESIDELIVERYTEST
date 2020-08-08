8e709d233282ac43ef920a0b8d32b57d13ccc368
alejandro <dilanantezana@gmail.com>
2020-08-08 18:10:34 -0400

    softdelete store

--- src/routes/stores.js
+++ src/routes/stores.js
@@ -24,9 +24,7 @@ stores.get('', (req, res, next) => {
     console.log({body}) 
     if(body !=   undefined){
         const {storeName} = req.query
-        Store.findOne({
-            name: storeName
-        })
+        Store.findOne({name: storeName}).isDeleted(false)
         .then(storeFound => {
             if (storeFound)
                 res.status(200).json(storeFound)
@@ -73,10 +71,10 @@ stores.put("/:storeID",(req, res, next) => {
         let response = {}
         Store.findOne({
             _id: id
-        })
+        }).isDeleted(false)
         .then(storeFound => {
             if (storeFound){
-                storeFound.softdelete(function(err, newTest) {
+                storeFound.softdelete(function(err) {
                     if (err) { res.json(err) }  
                   });
                 console.log("si se borro")
@@ -89,48 +87,28 @@ stores.put("/:storeID",(req, res, next) => {
         .catch(err => {
             console.error(err)
             res.status(500).json(err)
-        })/*
-        Store.findByIdAndDelete({_id:id}, function(err, docs) {
-            if(!err){
-                response.nosql = docs         
-                console.log(response)
-                return docs
-            }
-        }).then(() => {
-            response.msg = 'store delete'
-            res.status(200).send(response)
         })
-        .catch(err => {
-            console.warn(err)
-            res.status(500).send({ msg: 'Error on delete the store' })
-        })*/
     } else{
         res.status(400).send({ msg: 'No data' })
     }
-});
-stores.get("/get/info/", mdJWT, (req, response) => {
-    response.status(200).json({
-        msg: 'ok'
-    })
 });
 //actualizado en cascada
 stores.patch("/:storeID",(req,res,next)=>{
     const{ storeID : id} = req.params
     const {body} = req
-    //console.log(body)
+    console.log(body)
     if(req.body){
         let response = {}
         Store.findByIdAndUpdate(
             {_id:id},
-            {
-                name: req.body.name,   
-            },
+            {name: req.body.name},
             function(err, result) {
                 if (!err) {
                     response.anterior = result
                 }
             }
-        ).then(storeUpdated=> {
+        ).isDeleted(false)
+        .then(storeUpdated=> {
             response.nuevo = storeUpdated
             response.msg = 'store updated'
             res.status(200).send(response)
